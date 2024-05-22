103cda1d8c3c7decb18f517ac3d66fc1e4294399
Lucas Eduardo <lesds2001@gmail.com>
2024-05-21 22:35:26 -0300

    fazendo leitura com bufferedREader

---
+++ testeFiles/src/testeFiles/LendoComBuffersFileReder.java
@@ -0,0 +1,30 @@
+package testeFiles;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LendoComBuffersFileReder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String path = "c:\\temp\\medicos.json";
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+
+}
